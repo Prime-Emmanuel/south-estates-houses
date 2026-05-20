3b2fc0a7baa3245af3801cb142d01f001b88306f
Prime-Emmanuel <malongueprime@gmail.com>
2026-05-20 22:37:34 +0100

    Update api.js

--- server/functions/api.js
+++ server/functions/api.js
@@ -1,4 +1,7 @@
 import serverless from 'serverless-http';
 import app from '../src/index.js';
 
-export const handler = serverless(app);
+export const handler = serverless(app, {
+  framework: 'express',   // force Express
+  provider: 'aws'         // Netlify uses AWS‑style events
+});

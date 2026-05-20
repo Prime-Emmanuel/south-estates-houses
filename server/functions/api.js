3271d9cbf32360ef8cf8620ff92f7dea30516fd2
Prime-Emmanuel <malongueprime@gmail.com>
2026-05-20 23:19:27 +0100

    Delete server directory

--- server/functions/api.js
+++
@@ -1,9 +0,0 @@
-import serverless from 'serverless-http';
-import app from '../src/index.js';
-
-// Force Express framework detection
-export const handler = serverless(app, {
-  framework: 'express',
-  binary: true,
-  provider: 'aws'
-});
